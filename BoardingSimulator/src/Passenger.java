42726867b944d5823d1468d0ac2e2dbbb16b77b2
Aryan Keskar <66960583+AryanKeskar099@users.noreply.github.com>
2021-05-07 09:50:29 -0700

    created method bodies

--- BoardingSimulator/src/Passenger.java
+++ BoardingSimulator/src/Passenger.java
@@ -9,12 +9,15 @@ public class Passenger {
 	private int seatLocationX;
 	private int seatLocationY;
 	
+	private boolean isSeated;
+	
 	public Passenger(int x, int y, int radius, Color boardingGroup, int seatLocationX, int seatLocationY) {
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
 		this.seatLocationX = seatLocationX;
 		this.seatLocationY = seatLocationY;
+		isSeated = false;
 		
 		speed = (int)((Math.random() * 10) +1);
 	}
@@ -24,5 +27,18 @@ public class Passenger {
 		marker.circle(x, y, radius);
 	}
 	
-
+	private void walk() {
+		
+	}
+	public void act() {
+	}
+	
+	private void sit() {
+		
+	}
+	private void locateSeat() {
+		
+	}
+	
+	
 }
