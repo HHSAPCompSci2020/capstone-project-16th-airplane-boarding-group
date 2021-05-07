86962d2812c423be86fdcdabd8f26f6192878bbd
Aryan Keskar <66960583+AryanKeskar099@users.noreply.github.com>
2021-05-07 09:41:16 -0700

    created the draw method in passenger

--- BoardingSimulator/src/Passenger.java
+++ BoardingSimulator/src/Passenger.java
@@ -5,6 +5,7 @@ public class Passenger {
 	
 	private int x, y;
 	private int radius;
+	private int speed;
 	private Color boardingGroup;
 	
 	public Passenger(int x, int y, int radius, Color boardingGroup) {
@@ -12,6 +13,13 @@ public class Passenger {
 		this.y = y;
 		this.radius = radius;
 		this.boardingGroup = boardingGroup;
+		speed = (int)((Math.random() * 10) +1);
 	}
+	
+	public void draw(PApplet marker) {
+		marker.fill(0);
+		marker.circle(x, y, radius);
+	}
+	
 
 }
