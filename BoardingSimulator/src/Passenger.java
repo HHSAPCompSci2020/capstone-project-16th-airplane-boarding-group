4c1d761ed3ec4ef73093432ced40bcbb5892e3b8
Aryan Keskar <66960583+AryanKeskar099@users.noreply.github.com>
2021-05-07 09:28:49 -0700

    made constructor for passenger

--- BoardingSimulator/src/Passenger.java
+++ BoardingSimulator/src/Passenger.java
@@ -1,4 +1,17 @@
+import java.awt.Color;
+import processing.core.PApplet;
 
 public class Passenger {
+	
+	private int x, y;
+	private int radius;
+	private Color boardingGroup;
+	
+	public Passenger(int x, int y, int radius, Color boardingGroup) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.boardingGroup = boardingGroup;
+	}
 
 }
