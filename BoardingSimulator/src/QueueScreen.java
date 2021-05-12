d13c96c3979d87b501629a1501ce92aa42d43be5
Aryan Keskar <66960583+AryanKeskar099@users.noreply.github.com>
2021-05-12 09:25:18 -0700

    made method headers

--- BoardingSimulator/src/QueueScreen.java
+++ BoardingSimulator/src/QueueScreen.java
@@ -1,10 +1,26 @@
 
 public class QueueScreen extends Screen{
 	
+	//screen no 2
+	
 	private DrawingSurface surface;
+	
+	boolean 
 	
 	public QueueScreen(DrawingSurface marker) {
 		surface = marker;
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void mousePressed(int mouseX, int mouseY) {
+		
+	}
+	
+	public int mouseRelased() {
+		
 	}
 
 }
