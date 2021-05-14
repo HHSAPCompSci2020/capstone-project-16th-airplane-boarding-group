854dd818ed04483d9dfa79867dbe176a50de2866
Vamsee Krishna Vusuwandla <vvusuwandla605@student.fuhsd.org>
2021-05-14 09:13:14 -0700

    Timer update

--- BoardingSimulator/src/Timer.java
+++ BoardingSimulator/src/Timer.java
@@ -8,8 +8,13 @@ public class Timer {
 	private int screenWidth;
 	private int screenHeight;
 	private double seconds;
+	private double minutes;
 	
-	public Timer(int x,int y,int width, int height, int screenWidth, int screenHeight)
+	private boolean isRunning; 
+	private long start;
+	private long total = 0;
+	
+	public Timer(int x,int y,int width, int height, int screenWidth, int screenHeight, double seconds, double minutes)
 	{
 		this.x=x;
 		this.y=y;
@@ -17,7 +22,76 @@ public class Timer {
 		this.height=height;
 		this.screenWidth=screenWidth;
 		this.screenHeight=screenHeight;
+		this.seconds=seconds;
+		minutes = (int) (seconds/60);
 	}
 	
+	public void draw(PApplet marker) {
+		marker.fill(0);
+		marker.textSize(20);
+		marker.text(minutes + ":" + seconds, 35,40);
+		marker.rect(x, y, screenWidth, screenHeight);
+	}
+	public Timer()
+	  {
+	    start();
+	  }
+
+	public void start()
+	{
+	    if (isRunning) return;
+	    start = System.currentTimeMillis();
+	    isRunning = true;
+	  }
+
+	  public long stop()
+	  {
+	    if (isRunning) {
+	      updateTotalTime();
+	      isRunning = false;
+	    }
+	    return total;
+	  }
+
+	  public void reset()
+	  {
+	    total = 0;
+	    start = System.currentTimeMillis();
+	  }
+
+	  public long split()
+	  {
+	    if (isRunning)
+	      updateTotalTime();
+	    return total;
+	  }
+
+	  private void updateTotalTime()
+	  {
+	    long endTimestamp = System.currentTimeMillis();
+	    long elapsedTime = endTimestamp - start;
+	    start = endTimestamp;
+	    total += elapsedTime;
+	  }
+
+	  public long getTime()
+	  {
+	    updateTotalTime();
+	    return total;
+	  }
+
+	  
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
 
 }
