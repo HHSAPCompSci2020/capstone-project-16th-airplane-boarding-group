a88f7ff2d164a596cfe604a4d901112d1c993338
Vamsee Krishna Vusuwandla <vvusuwandla605@student.fuhsd.org>
2021-05-15 18:06:00 -0700

    updates 1

--- BoardingSimulator/src/Play.java
+++ BoardingSimulator/src/Play.java
@@ -1,17 +1,98 @@
+import java.awt.Color; 
+import processing.core.PApplet;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
 
 public class Play  extends Screen{
 	
+	Aircraft boeing747;
 	Timer timer = new Timer();
+	private Color[] boardingGroups;
+	private boolean createQueueButtonPressed;
+	private boolean createBoardingGroupPressed;
+	int queueX = 40;
+	int queueY = 40;
+	int queueLength = 100;
+	int queueHeight = 50;
+	int i=0;
 	
 	
 
 	public Play(DrawingSurface marker) {
 		super(marker);
+		boeing747 = new Aircraft();
+		setBoardingColors();
 		// TODO Auto-generated constructor stub
 	}
 	
 	public void draw() {
+		boeing747.draw(surface);
+		surface.rect(400,40,100,50);
+		surface.fill(0);
+		surface.text("click here to every time you want to make each boarding group, the first group is already set for red",350,40);
+		surface.text("you simply have to click individual seats to assign them to a boarding group",400,30);
+		surface.text("keep in mind that the maximum of groups you can use is 7, but you can use fewer to your liking",600,60);
+		
+		surface.fill(0);
+		surface.text("current boarding group:",200,40);
+		if(i<boardingGroups.length)
+		surface.fill(boardingGroups[i].getRed(),boardingGroups[i].getGreen(),boardingGroups[i].getBlue());
+		else {
+			surface.text("that's all the boarding groups we have set for you",100,100+i*10);
+		}
+		surface.rect(200,50,50,50);
 		timer.draw(surface);
+		if(createQueueButtonPressed)
+			surface.fill(255, 0, 0);
+		else
+			surface.fill(255);
+		
+		surface.rect(40, 40, 100, 50);
+		surface.fill(0);
+		surface.text("create queue", 40, 40);
+	}
+	
+	public void mousePressed(int mouseX, int mouseY) {
+		if(mouseX > queueX && mouseX < (queueLength + queueX) && mouseY > queueY && mouseY < (queueY + queueHeight)) {
+			createQueueButtonPressed = true;
+			
+		}
+		if(mouseX > 400 && mouseX < (500) && mouseY > 40 && mouseY < (90))
+		{
+			createBoardingGroupPressed= true;
+			i++;
+			System.out.println("color changed");
+		}
+		if(i<boardingGroups.length)
+		boeing747.seatClick(mouseX,mouseY,boardingGroups[i]);
+		
+		
+	}
+	
+	public int mouseReleased() {
+		return 0;
+	}
+	
+	public Color[] getBoardingGroup() {
+		Color [] trimmedGroups =Arrays.copyOfRange(boardingGroups,0,i);
+		return trimmedGroups;
+	}
+
+	public void setBoardingColors()
+	{
+		boardingGroups = new Color[7];
+		boardingGroups[0]= Color.RED;
+		boardingGroups[1]= Color.GREEN;
+		boardingGroups[2]= Color.BLUE;
+		boardingGroups[3]= Color.BLACK;
+		boardingGroups[4]= Color.CYAN;
+		boardingGroups[5]= Color.YELLOW;
+		boardingGroups[6]= Color.LIGHT_GRAY;
+		
 	}
 
 }
--- BoardingSimulator/src/Timer.java
+++ BoardingSimulator/src/Timer.java
@@ -1,5 +1,12 @@
 import java.awt.Color;
 import processing.core.PApplet;
+/**
+ * The timer class that keeps track of the time spent in the program, once run
+ * 
+ * @author primary author: Vamsee_Krishna_Vusuwandla
+ * @version 5/14
+ *
+ */
 public class Timer {
 	
 	private int x,y;
@@ -14,6 +21,11 @@ public class Timer {
 	private long start;
 	private long total = 0;
 	
+	/**
+	 *
+	 * initializing values in timer
+	 * 
+	 */
 	public Timer(int x,int y,int width, int height, int screenWidth, int screenHeight, double seconds, double minutes)
 	{
 		this.x=x;
@@ -26,25 +38,39 @@ public class Timer {
 		this.total=total;
 		this.isRunning = false;
 	}
-	
+	/**
+	 *
+	 * Draw method for the timer
+	 * @param marker 
+	 */
 	public void draw(PApplet marker) {
 		marker.fill(0);
 		marker.textSize(20);
 		marker.text(start + ":" + total, 35,40);
 		marker.rect(x, y, screenWidth, screenHeight);
 	}
+	/**
+	 * starts timer constructor
+	 *  
+	 */
 	public Timer()
 	  {
 	    start();
 	  }
-
+	/**
+	 *
+	 * starts timer to run 
+	 */
 	public void start()
 	{
 	    if (isRunning) return;
 	    start = System.currentTimeMillis();
 	    isRunning = true;
 	  }
-
+	/**
+	 *
+	 * stops timer 
+	 */
 	  public long stop()
 	  {
 	    if (isRunning) {
@@ -53,20 +79,29 @@ public class Timer {
 	    }
 	    return total;
 	  }
-
+	  /**
+		 *
+		 * resets timer
+		 */
 	  public void reset()
 	  {
 	    total = 0;
 	    start = System.currentTimeMillis();
 	  }
-
+	  /**
+		 *
+		 * Uses the total time
+		 */
 	  public long split()
 	  {
 	    if (isRunning)
 	      updateTotalTime();
 	    return total;
 	  }
-
+	  /**
+		 *
+		 * Updates the total time
+		 */
 	  private void updateTotalTime()
 	  {
 	    long endTimestamp = System.currentTimeMillis();
@@ -74,7 +109,10 @@ public class Timer {
 	    start = endTimestamp;
 	    total += elapsedTime;
 	  }
-
+	  /**
+		 *
+		 * gets time 
+		 */
 	  public long getTime()
 	  {
 	    updateTotalTime();
