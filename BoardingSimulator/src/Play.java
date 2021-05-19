e56c2e3b6f7358bf4f37a90804b38498141a4b67
Aryan Keskar <66960583+AryanKeskar099@users.noreply.github.com>
2021-05-19 15:22:25 -0700

    modified the play class

--- BoardingSimulator/src/Play.java
+++ BoardingSimulator/src/Play.java
@@ -25,18 +25,59 @@ public class Play  extends Screen{
 		
 	}
 	
+	/**
+	 * sets the boardingGroups array equal to the colors array
+	 * @param colors: the color array and order of the bording groups
+	 */
 	public void setBoardingGroup(Color[] colors) {
+		
 		boardingGroups = colors;
-		//for()
+		for(int i= 0;i<colors.length;i++)
+		{
+			boardingGroups[i]=colors[i];
+			
+			
+		}
 	}
 	public void mousePressed() {
 		
 	}
-	
+	/**
+	 * called upon in the drawing surface sets basic methods
+	 * @param boeing
+	 */
 	public void setAirplane(Aircraft boeing) {
 		airplane = boeing;
-	}
+		passengers = new Passenger[airplane.getOccupiedSeats()];
+		//System.out.println("red number of seats: " + airplane.getOccupiedGroups(boardingGroups[0]));
+		setPassengerToGroups();
+
 	
+		
+		
+	}
+	/**
+	 * creates new passenger objects, gives different values according to the color of the boarding group. 
+	 */
+	private void setPassengerToGroups()
+	
+	{
+	int passengernumber=0;
+	for(int i=0; i<boardingGroups.length;i++)
+		{
+			for(int j=0; j<airplane.getOccupiedGroups(boardingGroups[i]);j++)
+			{
+				passengers[passengernumber]= new Passenger(100,100,boardingGroups[i]);
+				passengernumber++;
+				//System.out.println("j");
+			}
+		}
+	for(int i = 0; i < passengers.length; i++) {
+		System.out.println(passengers[i].getColor());
+	}
+		
+	}
+
 	
 
 }
