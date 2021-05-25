8411dceb16e8c64adce824fea83b86fa729da79e
Aryan Keskar <66960583+AryanKeskar099@users.noreply.github.com>
2021-05-24 21:12:13 -0700

    commented on passenger

--- BoardingSimulator/src/Passenger.java
+++ BoardingSimulator/src/Passenger.java
@@ -23,6 +23,13 @@ public class Passenger extends PApplet{
 	private int delay = 0;
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	processing.core.PImage photo; 
+	
+	/**
+	 * Initializes all the variables in passenger to the given values and their defaults
+	 * @param x x coordinate of the passenger
+	 * @param y y coordinate of the passenger
+	 * @param passengerColor boarding group color of the passenger
+	 */
 	public Passenger(int x, int y,Color passengerColor) {
 		this.x = x;
 		this.y = y;
@@ -32,7 +39,10 @@ public class Passenger extends PApplet{
 	
 		speed = 2;
 	}
-	
+	/**
+	 * Draws the passenger
+	 * @param marker
+	 */
 	public void draw(PApplet marker) {
 		
 		marker.fill(255,69,0);
@@ -45,7 +55,8 @@ public class Passenger extends PApplet{
 		}
 		
 	}
-
+	
+	
 	private void walk(int seatX, int seatY) 
 	{		
 		if(seatX - 5<= x && x <= seatX + 5) {
@@ -73,6 +84,13 @@ public class Passenger extends PApplet{
 		}
 		
 	}
+	
+	/**
+	 * makes the passenger walk up to the seat, it also makes the passenger pause at the right moments
+	 * @param seatX: x coordinate of the seat
+	 * @param seatY: y coordinate of the seat
+	 */
+
 	public void act(int seatX, int seatY) {
 		walk(seatX,  seatY);
 		
@@ -80,7 +98,11 @@ public class Passenger extends PApplet{
 	}
 	
 	
-
+	/**
+	 * checks if the passenger is colliding with another
+	 * @param other: another passenger
+	 * @return false if passenger is not colliding true otherwise
+	 */
 	
 	public boolean isColliding(Passenger other) {
 		
@@ -91,43 +113,87 @@ public class Passenger extends PApplet{
 			return false;
 		}
 	}
-	
+	/**
+	 * gets the radius
+	 * @return Radius
+	 */
 	public int getRadius() {
 		return RADIUS;
 	}
+	/**
+	 * gets the x coordinate
+	 * @return x
+	 */
 	public int getX() {
 		return x;
 	}
+	/**
+	 * gets the y coordinate
+	 * @return y
+	 */
 	public int getY() {
 		return y;
 	}
+	/**
+	 * gets the color of the boarding group
+	 * @return color of the boarding group
+	 */
 	public Color getColor() {
 		return passengerColor;
 	}
+	/**
+	 * gets the x coordinate of the seat
+	 * @return x coordinate of the seat
+	 */
 	public int getSeatX() {
 		return seatX;
 	}
+	/**
+	 * sets the x coordinate of the seat
+	 * @param x: x coordinate of the seat
+	 */
 	public void setSeatX(int x) {
 		seatX = x;
 	}
+	/**
+	 * gets the y coordinate of the seat
+	 * @return y coordinate of the seat
+	 */
 	public int getSeatY() {
 		return seatY;
 	}
+	/**
+	 * sets the y coordinate of the seat
+	 * @param y: x coordinate of the seat
+	 */
 	public void setSeatY(int y) {
 		seatY = y;
 	}
+	/**
+	 * sets the speed of the seat
+	 * @param newSpeed: speed of the passenger
+	 */
 	public void setSpeed(int newSpeed) {
 		speed = newSpeed;
-	
 	}
+	/**
+	 * gets the speed of the passenger
+	 * @return speed: returns the speed of the passenger
+	 */
 	public int getSpeed() {
 		return speed;
 	}
-
+	
+	/**
+	 * returns whether the passenger is seated or not
+	 * @return isSeated: whether the passenger is seated or not
+	 */
 	public boolean isSeated() {
 		return isSeated;
 	}
-
+	/**
+	 * sets  the isSeated value to the parameter
+	 */
 	public void setSeated(boolean isSeated) {
 		this.isSeated = isSeated;
 	}
