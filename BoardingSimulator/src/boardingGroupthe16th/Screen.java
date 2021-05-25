ffd94b87d7ac3301d56a301b8938a84b0fa56a4a
Aryan Keskar <66960583+AryanKeskar099@users.noreply.github.com>
2021-05-24 21:40:00 -0700

    commented on Screen

--- BoardingSimulator/src/boardingGroupthe16th/Screen.java
+++ BoardingSimulator/src/boardingGroupthe16th/Screen.java
@@ -5,30 +5,46 @@ public class Screen {
 	//a
 	protected DrawingSurface surface;
 	
-	
+	/**
+	 * initializes parameters
+	 * @param marker: used  for initializing variable
+	 */
 	public Screen(DrawingSurface marker) {
 		surface = marker;
 	}
-	
+	/**
+	 * sets up the screen
+	 */
 	public void setup() {
 		
 	}
-	
+	/**
+	 * draws the screen
+	 */
 	public void draw() {
 	}
-	
+	/**
+	 * called when mouse is pressed, reacts accordingly
+	 * @param mouseX: x coordinate of the mouse
+	 * @param mouseY: y coordinate of the mouse
+	 */
 	public void mousePressed(int mouseX, int mouseY) {
 		
 	}
 	
-	public void mouseMoved() {
-		
-	}
-	
+	/**
+	 * called when mouse is dragged, reacts accordingly
+	 * @param mouseX: x coordinate of the mouse
+	 * @param mouseY: y coordinate of the mouse
+	 */
 	public void mouseDragged(int mouseX, int mouseY) {
 		
 	}
 	
+	/**
+	 * called when mouse is released, reacts accordingly
+	 * @return: returns the index of the next screen
+	 */
 	public int mouseReleased() {
 		return 0;
 	}
